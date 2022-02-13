f1c4c7e13cf7e21bd200d8be6eec333bae005d33
osorosia <64416398+osorosia@users.noreply.github.com>
2022-02-13 23:11:05 +0900

    fix

--- pass_check.sh
+++ pass_check.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
-declare -a PASSWORD
-PASSWORD+=('test')
+check() {
+  login=$1
+  password=$2
 
-for num in $( seq 0 $(( ${#PASSWORD[@]} - 1)) )
-do
-  p=${PASSWORD[$num]}
-  echo "[$((num + 1))] $p"
-  echo $p | pwscore admin
-done
+  echo [$login] $password
+  echo $password | pwscore $login
+}
+
+
+echo NG case
+check rnishimo 'Arupaka42'
+check rnishimo 'arupaka42tokyo'
+check rnishimo 'ArupakaTokyo'
+check rnishimo 'AAAArupaka42Tokyo'
+check rnishimo 'rnishimo42Tokyo'
+check root 'Arupaka42'
+check root 'arupaka42tokyo'
+check root 'ArupakaTokyo'
+check root 'AAAArupaka42Tokyo'
+check root 'rnishimo42Tokyo'
+
+
+echo OK case
+check rnishimo 'Arupaka42Tokyo'
+check rnishimo 'ARUPAKA42TOKYO'
+check rnishimo 'AAArupaka42tokyo'
+check root 'Arupaka42Tokyo'
+check root 'ARUPAKA42TOKYO'
+check root 'AAArupaka42tokyo'
